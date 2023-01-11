78bca8bc16e150376cdef5e3c9fbb8c25a18a663
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-01-11 15:21:47 +0900

    Update #12865 solution

---
+++ 동적 계획법 1/16#12865_평범한 배낭/16#12865_평범한 배낭.py
@@ -0,0 +1,13 @@
+n, k = map(int,input().split())
+pd = [[0 for i in range(k+1)] for j in range(n+1)]
+
+for i in range(1,n+1):
+    w, v = map(int,input().split())
+    for j in range(1,k+1):
+        if j<w:
+            pd[i][j] = pd[i-1][j]
+        else:
+            pd[i][j] = max(v + pd[i-1][j-w], pd[i-1][j])
+
+print(pd)
+print(max(map(max,pd)))
