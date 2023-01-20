668d4d2698d6e630353ec875db98d80601e5650b
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-01-20 23:06:55 +0900

    update #10828 solution

---
+++ 스택/01#10828_스택/01#10828_스택.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(input())
+stack=[]
+for i in range(n):
+    a= list(map(str,sys.stdin.readline().split()))
+    if a[0]=="push":
+        stack.append(a[1])
+    if a[0]=="top":
+        if len(stack)==0:
+            print("-1")
+        else:
+            print(stack[-1])
+    if a[0]=="size":
+        print(len(stack))
+    if a[0]=="empty":
+        if len(stack)==0:
+            print("1")
+        else:
+            print("0")
+    if a[0]=="pop":
+        if len(stack)==0:
+            print("-1")
+        else:
+            print(stack[-1])
+            stack = stack[:-1]
