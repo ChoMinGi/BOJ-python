cb08d9b5c0d4d0404b94683b1df02499b25a6a1a
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-02-24 17:08:54 +0900

    Update #6549 solution

---
+++ 분할 정복/09#6549_히스토그램에서 가장 큰 직사각형/09#6549_히스토그램에서 가장 큰 직사각형.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+
+def divandconq(l,r):
+    if l==r:
+        return tc[l]
+    # 가운데있는 부피 최대 구하기
+    m=l+(r-l)//2
+    lp=m
+    rp=m+1
+    mH=min(tc[lp],tc[rp])
+    mW=2
+    mAM=mH*mW
+
+    while (l<lp or rp<r):
+        if l==lp:
+            rp+=1
+            cH=tc[rp]
+        elif r==rp:
+            lp-=1
+            cH=tc[lp]
+        elif tc[lp-1] > tc[rp+1]:
+            lp-=1
+            cH=tc[lp]
+        else:
+            rp+=1
+            cH=tc[rp]
+        mH = min(mH,cH)
+        mW+=1
+        mAM = max(mAM,mH*mW)
+
+    return max(divandconq(l,m),divandconq(m+1,r),mAM)
+
+while 1:
+    tc = list(map(int, input().split()))
+    if tc[0]==0:
+        break
+    print(divandconq(1,len(tc)-1))
