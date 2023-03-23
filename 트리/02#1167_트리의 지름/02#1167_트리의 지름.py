32b4817990221182043d110dc0807cc6fdcaf8f0
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-03-23 23:08:09 +0900

    Update #1167 solution

--- 트리/02#1167_트리의 지름/02#1167_트리의 지름.py
+++ 트리/02#1167_트리의 지름/02#1167_트리의 지름.py
@@ -1,17 +1,36 @@
 import sys
 from heapq import heappush, heappop
+
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
-n=int(input())
-tree = [[] for _ in range(n+1)]
+n = int(input())
+tree = [[] for _ in range(n + 1)]
 
 for _ in range(n):
-    td=list(map(int,input().split()))
-    for i in range(len(td[1:-1])//2):
-        tree[td[0]].append((td[i+1],td[i+2]))
+    td = list(map(int, input().split()))
+    for i in range(len(td[1:-1]) // 2):
+        tree[td[0]].append((td[i + 1], td[i + 2]))
 print(tree)
+distance = [[0] * (n + 1) for _ in range(n + 1)]
 
-def distra(s):
-    heap=[]
-    tree[s]
+
+def dijkstra(dest):
+    heap = []
+    heappush(heap, (0, dest))
+    while heap:
+        dist, now = heappop(heap)
+        if distance[dest][now] > dist:
+            continue
+        for i in tree[now]:
+            cost = dist + i[1]
+            if cost > distance[dest][i[0]]:
+                distance[dest][i[0]] = cost
+                heappush(heap, (cost, i[0]))
+        print(distance)
+
+
+for i in range(1, n + 1):
+    dijkstra(i)
+
+print(distance)
