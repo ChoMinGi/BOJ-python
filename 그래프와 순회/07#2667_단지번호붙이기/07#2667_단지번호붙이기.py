1e0e4006a75036e54956c456a17fd762b1d407c3
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-05-20 11:34:54 +0900

    Update 07#2667_단지번호붙이기.py

--- 그래프와 순회/07#2667_단지번호붙이기/07#2667_단지번호붙이기.py
+++ 그래프와 순회/07#2667_단지번호붙이기/07#2667_단지번호붙이기.py
@@ -1,32 +1,45 @@
 import sys
 from collections import deque
 
+around = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+
+def bfs(i, j, graph):
+    n = len(graph)
+    q = deque()
+    q.append((i, j))
+    graph[i][j] = 0
+    cnt = 1
+    while q:
+        td = q.popleft()
+        for i in around:
+            tdx = i[0] + td[0]
+            tdy = i[1] + td[1]
+            if tdx < 0 or tdy < 0 or tdx >= n or tdy >= n:
+                continue
+            if graph[tdx][tdy] == 1:
+                graph[tdx][tdy] = 0
+                q.append((tdx, tdy))
+                cnt += 1
+    return cnt
+
+
 input = sys.stdin.readline
 
 n = int(input())
-
-graph = [[str(0)] * (n + 1)]
-visited = [[0] * (n + 1) for i in range(n + 1)]
-
+graph = []
 for _ in range(n):
-    graph.append(list("0" + str(input().rstrip())))
+    graph.append(list(map(int, input().rstrip())))
 
-print(visited, graph)
 
 res = []
-around = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if graph[i][j] == 1 and visited[i][j] == 0:
-            visited[i][j] = 1
-            cnt += 1
-            q = deque()
-            q.append((i, j))
-            while q:
-                td = q.popleft()
-                for i in around:
-                    tdx = i[0] + td[0]
-                    tdy = i[1] + td[1]
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            res.append(bfs(i, j, graph))
 
-        res.append(cnt)
-        cnt = 0
+
+res.sort()
+print(len(res))
+for i in range(len(res)):
+    print(res[i])
